c43bba8c095a6a698a4616be88c8e7d1daf20f11
JC-Null <johncanzo@icloud.com>
2022-01-02 15:59:17 -0500

    Initial Commit

---
+++ RemoteImageSwiftUI/RemoteImageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteImageSwiftUIApp.swift
+//  RemoteImageSwiftUI
+//
+//  Created by John Canzoneri on 1/2/22.
+//
+
+import SwiftUI
+
+@main
+struct RemoteImageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
