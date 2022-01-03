35f85296708826ad4d0f7f68ef0c2772891b9c6b
JC-Null <johncanzo@icloud.com>
2022-01-03 13:10:20 -0500

    initial

--- RemoteImageSwiftUI/ContentView.swift
+++ RemoteImageSwiftUI/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // this is the store the image in a variable
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       // MARK: 1. BASIC
+    //AsyncImage(url: URL(string: imageURL))
+        
+        // MARK: 2. SCALE
+        //AsyncImage(url: URL(string: imageURL), scale: 3.0)
+        
+    // MARK: 3. PLACEHOLDER
+        AsyncImage(url: URL(string: imageURL)) { image in image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+          Image(systemName: "photo.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 128)
+                .foregroundColor(.purple)
+                .opacity(0.5)
+        }
+        .padding(40)
     }
 }
 
